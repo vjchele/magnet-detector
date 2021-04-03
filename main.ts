9db78577052d3ad2f2ba7b99982767982798dcca
Vijay Cheleenahalli <cnvijayrao@gmail.com>
2021-04-03 10:27:10 -0400

    Added a comment

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 let baseline = input.magneticForce(Dimension.Strength)
+/**
+ * This project can detect a magnet if it's near a Microbit
+ */
 basic.forever(function () {
     if (Math.abs(input.magneticForce(Dimension.Strength) - baseline) > 100) {
         basic.showIcon(IconNames.No)
