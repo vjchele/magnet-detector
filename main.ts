cd76152e9a7c8fbbc0144f408aedad470f2d3acc
Vijay Cheleenahalli <cnvijayrao@gmail.com>
2021-04-03 17:48:39 -0400

    Changed the icon

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ let baseline = input.magneticForce(Dimension.Strength)
  */
 basic.forever(function () {
     if (Math.abs(input.magneticForce(Dimension.Strength) - baseline) > 100) {
-        basic.showIcon(IconNames.No)
+        basic.showIcon(IconNames.Yes)
     } else {
         basic.clearScreen()
     }
